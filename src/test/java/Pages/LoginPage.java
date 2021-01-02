3cde17bdfa3b38d2a4f8f96c91ab954cd05a44a5
kemal durgun <68253109+kemalD11@users.noreply.github.com>
2021-01-02 15:58:00 +0100

    basqar 2 neue

---
+++ src/test/java/Pages/LoginPage.java
@@ -0,0 +1,71 @@
+package Pages;
+
+import Utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.concurrent.TimeUnit;
+
+public class LoginPage extends Parent {
+    public LoginPage() {
+        PageFactory.initElements(driver, this);
+    }
+
+    WebElement myElement;
+
+    @FindBy(id = "mat-input-0")
+    private WebElement username;
+
+    @FindBy(id = "mat-input-1")
+    private WebElement password;
+
+    @FindBy(css = "button[aria-label='LOGIN']")
+    private WebElement loginButton;
+
+    @FindBy(linkText = "Got it!")
+    private WebElement gotItBtn;
+
+    @FindBy(xpath = "//div[contains(text(),'Invalid')]")
+    private WebElement errorMessage;
+
+    @FindBy(xpath = "//div[@role='alertdialog']")
+    private WebElement errorAlert;
+
+    @FindBy(xpath = "//h3[text()='  Dashboard ']")
+    public WebElement dashboardText;
+
+    public void login(String usernameStr, String passwordStr) {
+        sendKeysFunction(username, usernameStr);
+        sendKeysFunction(password, passwordStr);
+        clickFunction(loginButton);
+
+        clickFunction(gotItBtn);
+
+
+    }
+
+    public void goToPage() {
+        driver = Driver.getDriver();
+        driver.get("https://test.basqar.techno.study/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    public void checkTheMessage(String ElementName, String message) {
+        switch (ElementName) {
+            case "errorMessage":
+                myElement = errorMessage;
+                break;
+
+            case "errorAlert":
+                myElement = errorAlert;
+                break;
+        }
+
+        verifyElementContainsText(myElement, message);
+    }
+}
+
+
---
+++ src/test/java/StepDefinitions/_00_LoginStepDefs.java
@@ -0,0 +1,45 @@
+package StepDefinitions;
+import Pages.LoginPage;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.openqa.selenium.WebDriver;
+
+public class _00_LoginStepDefs {
+
+
+
+    WebDriver driver;
+
+    @Given("^Navigate to basqar$")
+    public void navigateToBasqar() {
+        new LoginPage().goToPage();
+    }
+
+    @When("^Enter username and password and click Login Button$")
+    public void enterUsernameAndPasswordAndClickLoginButton() {
+        new LoginPage().login("daulet2030@gmail.com","TechnoStudy123@");
+    }
+
+
+//@Then("^User should login successfully$")
+//    public void userShouldLoginSuccessfully() {
+//
+//        dialogContent.findElementAndVerifyContainsText("dashboard","Dashboard");
+//
+//    }
+
+
+
+    @When("^Enter \"([^\"]*)\" and \"([^\"]*)\"$")
+    public void enterAnd(String username, String password){
+        new LoginPage().login(username,password);
+
+    }
+
+    @Then("^Error message \"([^\"]*)\" should be displayed$")
+    public void errorMessageShouldBeDisplayed(String message)  {
+        new LoginPage().checkTheMessage("errorAlert",message);
+
+    }
+}
